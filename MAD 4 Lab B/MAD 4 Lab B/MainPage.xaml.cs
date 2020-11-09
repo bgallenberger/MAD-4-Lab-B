b8188abe3b8884da6b9d60f9a86870a4e05a1421
Brian Gallenberger <BGallenberger@my.wctc.edu>
2020-11-09 15:03:56 -0600

    Add project files.

---
+++ MAD 4 Lab B/MAD 4 Lab B/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MAD_4_Lab_B
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            count++;
+            (sender as Button).Text = $"Clicked {count} times";
+        }
+    }
+}
